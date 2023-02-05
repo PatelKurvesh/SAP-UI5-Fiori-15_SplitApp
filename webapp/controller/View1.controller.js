acf609f152e368a63a6edef1edadf5d2ef9965b3
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-05 21:47:47 +0530

    View 2 and app added

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,12 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-15_SplitApp.controller.View1", {
+		
+		onInit : function(){
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData("model/splitApp.json");
+			this.getView().setModel(oModel);
+		}
 
 	});
 });
